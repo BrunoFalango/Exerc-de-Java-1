9d3f7171bc8ed30f3b107d797bd3874a9c1ca4c2
BrunoFalango <brunofalango1997@gmail.com>
2020-11-12 14:38:08 -0300

    Exercicio Pedido

---
+++ Meu_Hobbie/src/Hobbie/HelloWorld.java
@@ -0,0 +1,11 @@
+package Hobbie;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Gosto de jogar videogame e assistir video no youtube, \n"
+				+ "Não gosto de que me encha muito a paciência");
+
+	}
+
+}
